35a65880608f89115f22a40846817b6f8f537165
Levi Burner <leviburner@gmail.com>
2016-02-20 17:48:16 -0500

    Add top level name for simbody

--- simulator.py
+++ simulator.py
@@ -7,7 +7,7 @@ def simulate():
     if setup_finished:
         algorithm.loop()
     else:
-        algorithm.setup()
         body = SimBody()
+        algorithm.setup()
         global setup_finished
         setup_finished = True
