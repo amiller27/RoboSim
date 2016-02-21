88deb8b8db55f85467865e2d8903e258c286efb4
Aaron Miller <acmiller273@gmail.com>
2016-02-21 07:38:28 -0500

    Added pin numbers to algorithm

--- algorithm.py
+++ algorithm.py
@@ -1,4 +1,3 @@
-import bge
 from RoboSim.robot.proximitysensor import ProximitySensor
 from RoboSim.robot.steppermove import StepperMove
 
@@ -9,8 +8,8 @@ move = None
 def setup():
     print('Algorithm setup')
     global px_right, px_left, move
-    px_right = ProximitySensor(1)
-    px_left = ProximitySensor(2)
+    px_right = ProximitySensor(6)
+    px_left = ProximitySensor(13)
     move = StepperMove()
 
 i = 0
@@ -18,17 +17,21 @@ queue = []
 def loop():
     global i
     if queue:
-        (queue.pop())()
+        (queue.pop(0))()
         return
     #print('Algorithm loop')
     #print(px_right.read(), px_left.read())
     if px_right.read():
-        if px_left.read():
-            queue.extend(lambda : move.backward() for i in range(10))
-            queue.extend(lambda : move.right() for i in range(10))
-        else:
-            move.left()
+        queue.extend(lambda : move.backward() for i in range(10))
+        queue.extend(lambda : move.left() for i in range(10))
+        #else:
+            #print("moving left")
+            #move.right()
     elif px_left.read():
-        move.right()
+        print("moving right")
+        queue.extend(lambda : move.backward() for i in range(10))
+        queue.extend(lambda : move.right() for i in range(10))
+        #move.left()
     else:
+        print("moving forward")
         move.forward()
